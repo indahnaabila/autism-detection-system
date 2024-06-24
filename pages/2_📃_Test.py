e75755d5f1b2838d2d6bad3f2bd13e739d90639d
Indah Nabila Rachmah <aureliachrysanthemum@gmail.com>
2024-06-24 14:02:10 +0700

    Update 2_📃_Test.py

--- pages/2_📃_Test.py
+++ pages/2_📃_Test.py
@@ -11,6 +11,8 @@ import os
 from pathlib import Path
 from CNNneeds import extract_frames, process_images_in_folder, predict_folder, analyze_data_np
 import os
+from pathlib import Path
+
 os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
 st.title("ASD Test")
@@ -19,7 +21,7 @@ tab1, tab2, tab3, tab4 = st.tabs(["Personal Information", "Screening", "ASD Test
 with tab1:
     st.write("Enter your personal information here.")
 
-    filename_consent = "1. output consent.xlsx"
+    filename_consent = Path('1. output consent.xlsx')
 
     with st.form(key='consent_form'):
         st.write("Personal Information:")
@@ -89,7 +91,7 @@ with tab1:
         #st.success('Form data saved to Excel successfully!')
 
 with tab2:
-    filename_scq = "2. output scq.xlsx"
+    filename_scq = Path('2. output scq.xlsx')
 
     questions = [
         "1. Apakah dia sekarang bisa berbicara menggunakan frasa atau kalimat pendek? Jika tidak, lanjutkan ke Pertanyaan 8",
@@ -207,7 +209,7 @@ with tab3:
     st.title("Video Feature Extraction")
     video_file = st.file_uploader("Upload a video", type=["mp4", "avi"])
     sheet_name = st.text_input("Enter the sheet name for the Excel output", "VideoAnalysis")
-    output_file = "3. output calculation.xlsx"
+    output_file = Path('3. output calculation.xlsx')
     fps_value = st.number_input("Enter the FPS value for processing", min_value=1, value=1)
 
     if video_file is not None:
@@ -306,7 +308,7 @@ with tab3:
             # Display the first few rows of the DataFrame and its information
             video_analysis_data.head(), video_analysis_data.info()
             # Load the SVM model
-            svm_model_loaded = load("svm_model_rbf_fix.joblib")
+            svm_model_loaded = load(Path('svm_model_rbf_fix.joblib')
 
             video_analysis_data = video_analysis_data.drop(columns=['Unnamed: 0'])
 
