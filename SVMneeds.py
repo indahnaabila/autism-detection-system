5c4266b9342e8921d32135115ae67d87db6b4a40
Indah Nabila Rachmah <aureliachrysanthemum@gmail.com>
2024-06-24 19:00:14 +0700

    Update SVMneeds.py

--- SVMneeds.py
+++ SVMneeds.py
@@ -7,6 +7,7 @@ import pandas as pd
 from matplotlib import pyplot as plt
 from openpyxl import load_workbook
 from pathlib import Path
+import streamlit as st
 
 class ImagePreprocessor:
     def __init__(self, image):
