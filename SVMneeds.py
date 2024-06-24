b7f764c03e4e7b65e097122e4cae3cd6f3f22ba3
Indah Nabila Rachmah <aureliachrysanthemum@gmail.com>
2024-06-24 18:59:12 +0700

    Update SVMneeds.py

--- SVMneeds.py
+++ SVMneeds.py
@@ -52,31 +52,36 @@ class ImagePreprocessor:
     def show_image(self, image):
         plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
         plt.show()
-        
+
 class LandmarkExtractor:
     def __init__(self):
         self.detector = dlib.get_frontal_face_detector()
-        #self.predictor = dlib.shape_predictor(str(Path('shape_predictor_68_face_landmarks.dat')))
         self.predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
     
     def extract_landmarks(self, image):
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        faces = self.detector(gray)
-        for face in faces:
-            landmarks = self.predictor(image=gray, box=face)
-            return np.array([(landmarks.part(n).x, landmarks.part(n).y) for n in range(68)])
+        try:
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            st.write(f"Grayscale image shape: {gray.shape}, dtype: {gray.dtype}")
+            
+            faces = self.detector(gray)
+            st.write(f"Number of faces detected: {len(faces)}")
+
+            for face in faces:
+                landmarks = self.predictor(image=gray, box=face)
+                return np.array([(landmarks.part(n).x, landmarks.part(n).y) for n in range(68)])
+        except Exception as e:
+            st.error(f"Error in extract_landmarks: {e}")
         return None
     
     def visualize_landmarks(self, image, landmarks):
         if landmarks is None:
             print("No landmarks to visualize.")
             return
-        plt.figure(figsize=(8,8))
+        plt.figure(figsize=(8, 8))
         plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
         plt.scatter(landmarks[:, 0], landmarks[:, 1], s=20, marker='.', c='c')
-        #plt.axis('off')
         plt.show()
-
+        
 class FeatureCalculator:
     
     #Rumus Angle
